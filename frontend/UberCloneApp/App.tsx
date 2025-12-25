c3eef930bf68e01fcf1505d72b803494bfdcd8cf
sarniraula <sar.niraula162@gmail.com>
2025-12-25 14:24:09 +0545

    Frontend basic setup

---
+++ frontend/UberCloneApp/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Uber Clone App</Text>
+    <Text>Uber Clone App</Text>
+  </View>
+);
+
+export default App;
